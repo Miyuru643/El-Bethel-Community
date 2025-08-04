62fc1f0edafb4bb90cd01fbf34da4d41c026754d
Miyuru643 <sathsaramiyuru3000@gmail.com>
2025-08-04 18:04:28 +0530

    Add files via upload

---
+++ EBC_main_page_.js
@@ -0,0 +1,26 @@
+function
+        sinhalabi() {
+                window.location.href="Sinhala_.html"
+        }
+
+function
+        tamilbi() {
+                window.location.href="Tamil_.html"
+        }
+    
+function
+        podcasts() {
+                window.location.href="Podcasts_.html"
+        }
+
+function
+        songs() {
+                window.location.href="Songs_.html"
+        }
+
+function
+        contact() {
+                document.getElementById('contactp').innerHTML="Contact us: 075 016 0458";
+                document.getElementById('contactp').style.display="block";
+        }
+
